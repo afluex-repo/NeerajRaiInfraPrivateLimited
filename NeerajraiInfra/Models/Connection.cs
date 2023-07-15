dc065f4fd73fd2082e80fa73e57c9e08e085faea
Sudhakar Sharma <developer2.afluex@gmail.com>
2023-07-15 17:56:37 +0530

    changes

--- NeerajraiInfra/Models/Connection.cs
+++ NeerajraiInfra/Models/Connection.cs
@@ -17,8 +17,8 @@ namespace NeerajraiInfra.Models
         {
             try
             {
-                connectionString = "Data Source=103.48.51.111,1232;Initial Catalog= NeerajRaiInfraDB; User Id= neerajraiinfrauser; Password=Neeraj123!@#;Integrated Security=false;";
-                //connectionString = "Data Source=DESKTOP-02J59E3\\SQLEXPRESS;Initial Catalog=NeerajraiInfraDB;Integrated Security=True";
+                //connectionString = "Data Source=103.48.51.111,1232;Initial Catalog= NeerajRaiInfraDB; User Id= neerajraiinfrauser; Password=Neeraj123!@#;Integrated Security=false;";
+                connectionString = "Data Source=DESKTOP-02J59E3\\SQLEXPRESS;Initial Catalog=NeerajraiInfraDB;Integrated Security=True";
             }
             catch (Exception) 
             {
